4e4db4260f37846bb533a5676fafa90fea4c8ff3
ko-suke24 <kosuke0322@gmail.com>
2021-06-24 08:23:53 +0900

    Initial Commit

---
+++ login/loginApp.swift
@@ -0,0 +1,17 @@
+//
+//  loginApp.swift
+//  login
+//
+//  Created by 佐藤航輔 on 2021/06/24.
+//
+
+import SwiftUI
+
+@main
+struct loginApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
