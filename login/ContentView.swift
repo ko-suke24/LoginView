f9efb5a6f33610708fe11540081f09cca952c24a
ko-suke24 <kosuke0322@gmail.com>
2021-06-26 06:33:49 +0900

    first commit

--- login/ContentView.swift
+++ login/ContentView.swift
@@ -9,13 +9,60 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            WelcomeScreenView()
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct PrimaryButton: View {
+    var title: String
+    var body: some View {
+        Text(title)
+            .font(.title3)
+            .fontWeight(.bold)
+            .foregroundColor(.white)
+            .frame(maxWidth: .infinity)
+            .padding()
+            .background(Color("PrimaryColor"))
+            .cornerRadius(50)
+    }
+}
+
+struct WelcomeScreenView: View {
+    var body: some View {
+        ZStack {
+            Color("BgColor").edgesIgnoringSafeArea(.all)
+            VStack {
+                Spacer()
+                Image(uiImage: #imageLiteral(resourceName: "onboard"))
+                Spacer()
+                PrimaryButton(title: "Get Started")
+                
+                Text("Sign in")
+                    .font(.title3)
+                    .fontWeight(.bold)
+                    .foregroundColor(Color("PrimaryColor"))
+                    .padding()
+                    .frame(maxWidth: .infinity)
+                    .background(Color.white)
+                    .cornerRadius(50)
+                    .shadow(color: Color.black.opacity(0.08), radius: 60, x: 0.0, y: 16)
+                    .padding(.vertical)
+                
+                HStack {
+                    Text("New around here? ")
+                    Text("Sign in")
+                        .foregroundColor(Color("PrimaryColor"))
+                }
+            }
+            .padding()
+        }
     }
 }
